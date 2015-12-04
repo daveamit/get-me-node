35c6f0776fb66a06d90ab70d4d3ac661674c1857
Dave Amit <self@daveamit.com>
2015-12-05 02:54:54 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -64,7 +64,7 @@ source ~/.profile
 clear
 
 
-export NVM_DIR="~/.nvm"
+export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
 
 echo '------------------------------------------------------------------------------'
