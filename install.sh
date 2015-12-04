a17b3617a7e834a9b23dd82f5699209e1b9a5597
Dave Amit <self@daveamit.com>
2015-12-05 00:32:44 +0530

    download and install atom

--- install.sh
+++ install.sh
@@ -17,7 +17,21 @@ echo '--------------------------------------------------------------------------
 
 sudo apt-get -y install build-essential libssl-dev git zsh docky
 
+clear
 echo '------------------------------------------------------------------------------'
 echo 'Installing most awesomest of all shells - Oh-My-ZSH!'
 echo '------------------------------------------------------------------------------'
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+clear
+echo '------------------------------------------------------------------------------'
+echo 'Downloading atom!'
+echo '------------------------------------------------------------------------------'
+curl -o ~/.atom.deb "$(curl -fsSl https://atom.io/download/deb  | grep -o '<a .*href=.*>' | sed -e 's/<a /\n<a /g' | sed -e 's/<a .*href=['"'"'"]//' -e 's/["'"'"'].*$//' -e '/^$/ d')"
+clear
+echo '------------------------------------------------------------------------------'
+echo 'Installing atom!'
+echo '------------------------------------------------------------------------------'
+sudo dpkg -i ~/.atom.deb
+
+
