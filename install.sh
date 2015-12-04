1d975036b36eed1fd74bcfd3839aab677cdf1fda
Dave Amit <self@daveamit.com>
2015-12-05 02:23:25 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -62,6 +62,7 @@ echo '--------------------------------------------------------------------------
 curl https://raw.githubusercontent.com/creationix/nvm/v0.29.0/install.sh | bash
 source ~/.profile
 clear
+exit
 
 echo '------------------------------------------------------------------------------'
 echo 'Installing common node version, 5.1.0, 5.0.0 and 0.12.7!'
