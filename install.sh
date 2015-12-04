016f7958b36ac15b77cdfa8555569195c20f6123
Dave Amit <self@daveamit.com>
2015-12-04 23:49:08 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,4 +5,19 @@ echo 'Sit back and relax while we setup node and related stuff to your machine'
 echo '------------------------------------------------------------------------------'
 
 sudo apt-get -y update
+clear
+echo '------------------------------------------------------------------------------'
+echo 'Now upgrading your machine'
+echo '------------------------------------------------------------------------------'
 sudo apt-get -y upgrade
+clear
+echo '------------------------------------------------------------------------------'
+echo 'Awesome! Installing few packages which we'll need
+echo '------------------------------------------------------------------------------'
+
+sudo apt-get -y install build-essential libssl-dev git zsh docky
+
+echo '------------------------------------------------------------------------------'
+echo 'Installing most awesomest of all shells - Oh-My-ZSH!
+echo '------------------------------------------------------------------------------'
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
