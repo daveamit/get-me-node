bdaa9a27c5ab34b572dd893c9eeb19e366dfd8c4
Dave Amit <self@daveamit.com>
2015-12-05 01:46:02 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -23,11 +23,31 @@ echo 'Installing most awesomest of all shells - Oh-My-ZSH!'
 echo '------------------------------------------------------------------------------'
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-clear
-echo '------------------------------------------------------------------------------'
-echo 'Downloading atom!'
-echo '------------------------------------------------------------------------------'
+if [ -f "~/.atom.deb" ];
+then
+  clear
+  echo '------------------------------------------------------------------------------'
+  echo 'Atom already downloaded.!'
+  echo '------------------------------------------------------------------------------'
+  read -p "Do you wish to force download latest version of atom from atom.io? Enter No to install from downloaded version or Yes to download latest version: [Yes/No] " yn
+  if[$yn = "Yes"]
+  then
+      clear
+      echo '------------------------------------------------------------------------------'
+      echo 'Downloading atom!'
+      echo '------------------------------------------------------------------------------'
+      curl -o ~/.atom.deb "$(curl -fsSl https://atom.io/download/deb  | grep -o '<a .*href=.*>' | sed -e 's/<a /\n<a /g' | sed -e 's/<a .*href=['"'"'"]//' -e 's/["'"'"'].*$//' -e '/^$/ d')"
+  else
+      clear
+      echo 'not downloading from atom.io, using chached copy'
+  fi
+else
+  
+  echo '------------------------------------------------------------------------------'
+  echo 'Downloading atom!'
+  echo '------------------------------------------------------------------------------'
 curl -o ~/.atom.deb "$(curl -fsSl https://atom.io/download/deb  | grep -o '<a .*href=.*>' | sed -e 's/<a /\n<a /g' | sed -e 's/<a .*href=['"'"'"]//' -e 's/["'"'"'].*$//' -e '/^$/ d')"
+fi
 clear
 echo '------------------------------------------------------------------------------'
 echo 'Installing atom!'
@@ -35,3 +55,5 @@ echo '--------------------------------------------------------------------------
 sudo dpkg -i ~/.atom.deb
 
 
+
+ 
