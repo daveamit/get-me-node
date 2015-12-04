252ad1a49dd37abdd4d7c19546bc89fc20be6345
Dave Amit <self@daveamit.com>
2015-12-04 23:40:46 +0530

    apt-get now won't ask for confirmations

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-echo 'updating ubuntu'
-sudo apt-get update
-sudo apt-get upgrade
+echo '------------------------------------------------------------------------------'
+echo 'Sit back and relax while we setup node and related stuff to your machine'
+echo '------------------------------------------------------------------------------'
+
+sudo apt-get -y update
+sudo apt-get -y upgrade
